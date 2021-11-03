354930b6001d96e55c8248fd38f2744d026adda2
choochoo <90181966+hyunchoo@users.noreply.github.com>
2021-11-03 17:00:35 +0900

    Add files via upload

---
+++ day_11/day_11/AddController.swift
@@ -0,0 +1,29 @@
+//
+//  AddController.swift
+//  day_11
+//
+//  Created by lee on 2021/11/01.
+//
+
+import UIKit
+
+class AddController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
